265f2909178f309504894b0d128d8ad7cd3f4689
Kashish Jain <kashishj.msc25@cs.du.ac.in>
2026-08-14 00:08:58 +0530

    Update dependencies configuration

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,13 +1,39 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.core.security import decode_access_token
 from app.models.user import User, UserRole
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+#oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+security = HTTPBearer()
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: Session = Depends(get_db)
+) -> User:
+
+    exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    token = credentials.credentials
+
+    payload = decode_access_token(token)
+
+    if not payload or "sub" not in payload:
+        raise exc
+
+    user = db.query(User).filter(User.id == payload["sub"]).first()
+
+    if not user or not user.is_active:
+        raise exc
+
+    return user
+
+"""def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
     exc = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
     payload = decode_access_token(token)
     if not payload or "sub" not in payload:
@@ -15,7 +41,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     user = db.query(User).filter(User.id == payload["sub"]).first()
     if not user or not user.is_active:
         raise exc
-    return user
+    return user """
 
 def require_roles(*roles: UserRole):
     def dependency(current_user: User = Depends(get_current_user)):
