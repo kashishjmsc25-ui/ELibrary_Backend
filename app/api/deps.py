97797472a8595b8671a6334105e750b6ec3b6711
Kashish Jain <kashishj.msc25@cs.du.ac.in>
2026-08-14 03:22:44 +0530

    Update deps.py

--- app/api/deps.py
+++ app/api/deps.py
@@ -5,7 +5,6 @@ from app.database import get_db
 from app.core.security import decode_access_token
 from app.models.user import User, UserRole
 
-#oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
 security = HTTPBearer()
 
 def get_current_user(
@@ -32,16 +31,6 @@ def get_current_user(
         raise exc
 
     return user
-
-"""def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
-    exc = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
-    payload = decode_access_token(token)
-    if not payload or "sub" not in payload:
-        raise exc
-    user = db.query(User).filter(User.id == payload["sub"]).first()
-    if not user or not user.is_active:
-        raise exc
-    return user """
 
 def require_roles(*roles: UserRole):
     def dependency(current_user: User = Depends(get_current_user)):
